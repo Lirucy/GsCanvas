274e4d38685f3635bb02f543e011b3245b255a84
lirucy <rick_hertel77@msn.com>
2021-11-03 11:22:20 -0600

    add default route

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,2 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
+const apiURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:8000" : "produrl";
+
+axios.defaults.withCredentials = true;
+
+export const defaultRoute = async () => {
+  try {
+    const response = await axios.get(apiURL);
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
