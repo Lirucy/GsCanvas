5001e0a1218e22221da566557ab651ec9d68a964
lirucy <rick_hertel77@msn.com>
2021-11-03 21:50:00 -0600

    create userProfile component, filter comments, pass to profileComment component

---
+++ client/src/screens/UserProfile.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { getAllComments } from "../services";
+
+const UserProfile = (props) => {
+    const [filteredComments, setFilteredComments] = useState([])
+
+    useEffect(() => {
+        const fetchComments = async () => {
+            const fetchedComments = await getAllComments()
+            const filtered = fetchedComments.filter(
+                (comment) => comment.user.id === props.user?.id
+            )
+            setFilteredComments(filtered)
+        }
+        fetchedComments()
+    }, [props.user])
+
+    return (
+        <section id="user-profile-body">
+            <h2>My Comments</h2>
+            <article>
+                {/* {filteredComments.map((comment) => (
+                    <ProfileComment key={comment.id} comment={comment}/>
+                ))} */}
+            </article>
+        </section>
+    );
+};
+
+export default UserProfile;
