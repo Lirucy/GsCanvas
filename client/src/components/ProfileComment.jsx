29d395f5bc67971828d1a4df4f3980a089feb635
lirucy <rick_hertel77@msn.com>
2021-11-04 12:07:40 -0600

    add user profile, user specific images

--- client/src/components/ProfileComment.jsx
+++ client/src/components/ProfileComment.jsx
@@ -4,11 +4,12 @@ import getOnePiece from "../services";
 
 
 const ProfileComment = (props) => {
-    const { comment } = props.comment
+    const { comment, art_id } = props.comment
     // const [artPiece, setArtPiece] = useState({})
 
     return (
         <article>
+            <img src={art_id.url}/>
             <p>{comment}</p>
             <Link to={`/edit-comment/${props.comment.id}`}><button>Edit Comment</button></Link>
         </article>
