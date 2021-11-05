06481b467af613f5f28d0b4d0adc50e6cfc196ac
lirucy <rick_hertel77@msn.com>
2021-11-05 14:13:42 -0600

    add grid to bio component

--- client/src/screens/Bio.jsx
+++ client/src/screens/Bio.jsx
@@ -4,9 +4,8 @@ const Bio = () => {
     return (
         <section id="bio-body">
             <h2>Artist: Gianna Hertel</h2>
-            <img src="https://iili.io/50KOR2.png" alt="Gianna Hertel"/>
-            <p>Gianna is a young artist with a growing desire to inspire. From a young age, she was eager to share her view of the world through her art. Her focus has always been to create art that causes the viewer to contemplate the deeper meaning. Her creativity and delivery continues to grow and evolve with each and every creation.</p>
-            <p>If you like what you see, feel free to register and comment on your favorite pieces!</p>
+            <img id="bio-pic" src="https://iili.io/50KOR2.png" alt="Gianna Hertel"/>
+            <p id="bio-desc">Gianna is a young artist with a growing desire to inspire. From a young age, she was eager to share her view of the world through her art. Her focus has always been to create art that causes the viewer to contemplate the deeper meaning. Her creativity and delivery continues to grow and evolve with each and every creation.<br/><br/>If you like what you see, feel free to <a href="/register" >~register~</a> and comment on your favorite pieces!</p>
         </section>
     );
 };
