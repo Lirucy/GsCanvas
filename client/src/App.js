70313b2060f8b7c7b4da947a6da11224e78541e1
lirucy <rick_hertel77@msn.com>
2021-11-05 11:38:17 -0600

    style body background bio component, bio.css

--- client/src/App.js
+++ client/src/App.js
@@ -2,48 +2,57 @@ import { Link, Route, Switch } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Home from "./screens/Home";
 import Nav from "./components/Nav";
-import ArtDetail from "./screens/ArtDetail"
+import ArtDetail from "./screens/ArtDetail";
 import Bio from "./screens/Bio";
 import EditComment from "./screens/EditComment";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import UserProfile from "./screens/UserProfile";
 import "./App.css";
-import "./css/ArtDetail.css"
-import "./css/EditComment.css"
-import "./css/Home.css"
-import "./css/Login.css"
-import "./css/Nav.css"
-import "./css/Register.css"
+import "./css/ArtDetail.css";
+import "./css/Bio.css";
+import "./css/EditComment.css";
+import "./css/Home.css";
+import "./css/Login.css";
+import "./css/Nav.css";
+import "./css/Register.css";
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
     <div className="App">
-      <Nav user={user} setUser={setUser}/> 
-      <div className="divider" style={{color: "darkred"}}>-</div>
-      <Link to="/bio"><div id="app-title" className="fade">G's Canvas</div></Link>
-      <div className="divider" style={{color: "darkred"}}>-</div>
+      <Nav user={user} setUser={setUser} />
+      <div className="divider" style={{ color: "darkred" }}>
+        -
+      </div>
+      <Link to="/bio">
+        <div id="app-title" className="fade">
+          G's Canvas
+        </div>
+      </Link>
+      <div className="divider" style={{ color: "darkred" }}>
+        -
+      </div>
       <Switch>
         <main>
           <Route exact path="/">
             <Home />
           </Route>
           <Route path="/register">
-            <Register setUser={setUser}/>
+            <Register setUser={setUser} />
           </Route>
           <Route path="/login">
-            <Login setUser={setUser}/>
+            <Login setUser={setUser} />
           </Route>
           <Route path="/user-profile">
-            <UserProfile user={user}/>
+            <UserProfile user={user} />
           </Route>
           <Route path="/edit-comment/:id">
             <EditComment />
           </Route>
           <Route path="/art/:id">
-            <ArtDetail user={user}/>
+            <ArtDetail user={user} />
           </Route>
           <Route path="/bio">
             <Bio />
--- client/src/screens/Bio.jsx
+++ client/src/screens/Bio.jsx
@@ -2,7 +2,7 @@
 
 const Bio = () => {
     return (
-        <section>
+        <section id="bio-body">
             <h2>Artist: Gianna Hertel</h2>
             <img src="https://iili.io/50KOR2.png" alt="Gianna Hertel"/>
             <p></p>
