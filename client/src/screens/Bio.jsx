3eef6ac23ced93a345ffd6152fcee8935d133679
lirucy <rick_hertel77@msn.com>
2021-11-05 13:01:28 -0600

    add bio description

--- client/src/screens/Bio.jsx
+++ client/src/screens/Bio.jsx
@@ -5,7 +5,8 @@ const Bio = () => {
         <section id="bio-body">
             <h2>Artist: Gianna Hertel</h2>
             <img src="https://iili.io/50KOR2.png" alt="Gianna Hertel"/>
-            <p></p>
+            <p>Gianna is a young artist with a growing desire to inspire. From a young age, she was eager to share her view of the world through her art. Her focus has always been to create art that causes the viewer to contemplate the deeper meaning. Her creativity and delivery continues to grow and evolve with each and every creation.</p>
+            <p>If you like what you see, feel free to register and comment on your favorite pieces!</p>
         </section>
     );
 };
