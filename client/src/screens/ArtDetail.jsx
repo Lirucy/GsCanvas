3b82a46d1120b77e8575780aab56b98d8d4eb496
lirucy <rick_hertel77@msn.com>
2021-11-06 06:50:28 -0600

    add styling to art detail component

--- client/src/screens/ArtDetail.jsx
+++ client/src/screens/ArtDetail.jsx
@@ -3,58 +3,62 @@ import { useParams, useHistory } from "react-router-dom";
 import { getOnePiece, createComment } from "../services";
 
 const ArtDetail = (props) => {
-    // const { username } = props.user;
-    const [artPiece, setArtPiece] = useState({})
-    const [comment, setComment] = useState("")
-    const params = useParams();
-    const artId = params.id;
-    const history = useHistory()
+  // const { username } = props.user;
+  const [artPiece, setArtPiece] = useState({});
+  const [comment, setComment] = useState("");
+  const params = useParams();
+  const artId = params.id;
+  const history = useHistory();
 
-    useEffect(() => {
-        const fetchArtById = async () => {
-            const currentPiece = await getOnePiece(artId);
-            setArtPiece(currentPiece);
-        }
-        fetchArtById();
-    }, [artId])
+  useEffect(() => {
+    const fetchArtById = async () => {
+      const currentPiece = await getOnePiece(artId);
+      setArtPiece(currentPiece);
+    };
+    fetchArtById();
+  }, [artId]);
 
-    const clickHandler = async (e) => {
-        e.preventDefault();
-        const newComment = {
-            comment
-        } 
-        await createComment(newComment, artId)
-        history.push("/user-profile")
-    }
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    const newComment = {
+      comment,
+    };
+    await createComment(newComment, artId);
+    history.push("/user-profile");
+  };
 
-    return (
-        <div id="detail-body">
-            <article>
-                <h3>{artPiece?.title}</h3>
-                <img className="fade" src={artPiece?.url} alt="art piece"/>
-                <h3>Comments:</h3>
-                {artPiece?.comments?.map((comment) => (
-                    <p>~{comment.user.username}~<br/>{comment.comment}</p>
-                ))}
-                {props.user ? (
-                  <>
-                  <input
-                id="comment"
-                type="text"
-                required
-                placeholder="comment here"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                />
-                <button className="fade" type="submit" onClick={clickHandler}>Comment</button>
-                  </>
-                ) : (
-                  <>
-                  </>
-                )}
-            </article>
-        </div>
-    );
+  return (
+    <div id="detail-body">
+      <article>
+        <h3>{artPiece?.title}</h3>
+        <img className="fade" src={artPiece?.url} alt="art piece" />
+        <h3>Comments:</h3>
+        {artPiece?.comments?.map((comment) => (
+          <p>
+            ~{comment.user.username}~<br />
+            {comment.comment}
+          </p>
+        ))}
+        {props.user ? (
+          <>
+            <input
+              id="comment"
+              type="text"
+              required
+              placeholder="comment here"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button className="fade" type="submit" onClick={clickHandler}>
+              Comment
+            </button>
+          </>
+        ) : (
+          <></>
+        )}
+      </article>
+    </div>
+  );
 };
 
 export default ArtDetail;
