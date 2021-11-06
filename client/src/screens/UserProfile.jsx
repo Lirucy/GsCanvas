6db689f37fec768acf416217a8acd311fd4224ed
lirucy <rick_hertel77@msn.com>
2021-11-06 06:16:27 -0600

    add styling to user profile page

--- client/src/screens/UserProfile.jsx
+++ client/src/screens/UserProfile.jsx
@@ -1,32 +1,29 @@
 import { useEffect, useState } from "react";
 import { getAllComments } from "../services";
-import ProfileComment from "../components/ProfileComment"; 
+import ProfileComment from "../components/ProfileComment";
 
 const UserProfile = (props) => {
-    const [filteredComments, setFilteredComments] = useState([])
+  const [filteredComments, setFilteredComments] = useState([]);
 
-    useEffect(() => {
-        const fetchComments = async () => {
-            const fetchedComments = await getAllComments()
-            const filtered = fetchedComments.filter(
-                (comment) => comment.user.id === props.user?.id
-            )
-            console.log(fetchedComments)
-            setFilteredComments(filtered)
-        }
-        fetchComments()
-    }, [props.user])
+  useEffect(() => {
+    const fetchComments = async () => {
+      const fetchedComments = await getAllComments();
+      const filtered = fetchedComments.filter(
+        (comment) => comment.user.id === props.user?.id
+      );
+      console.log(fetchedComments);
+      setFilteredComments(filtered);
+    };
+    fetchComments();
+  }, [props.user]);
 
-    return (
-        <section id="profile-body">
-            <h2>My Comments</h2>
-            <article>
-                {filteredComments.map((comment) => (
-                    <ProfileComment key={comment.id} comment={comment}/>
-                ))}
-            </article>
-        </section>
-    );
+  return (
+      <section id="profile-body">
+        {filteredComments.map((comment) => (
+          <ProfileComment key={comment.id} comment={comment} />
+        ))}
+      </section>
+  );
 };
 
 export default UserProfile;
